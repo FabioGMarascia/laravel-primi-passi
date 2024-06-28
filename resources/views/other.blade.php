8a732a71ca4e2ce5b75b6da442d0e309d4adf8fd
FabioGMarascia <fabiogaspare.marascia@gmail.com>
2024-06-28 10:16:21 +0200

    css in public

--- resources/views/other.blade.php
+++ resources/views/other.blade.php
@@ -7,10 +7,11 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous" />
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous">
     </script>
+    <link rel="stylesheet" href="/style.css">
     <title>LARAVEL</title>
 </head>
 
-<body class="bg-black pt-5">
+<body class="pt-5">
 
     <h1 class="display-1 fw-bold text-warning text-center my-4 text-center mt-5">HELLO LARAVEL OTHER PAGE</h1>
 
